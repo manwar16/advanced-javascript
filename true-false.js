484e2caf076d57495d03270e2b9e84511a905a14
manwar16 <mdmanwarcse16@gmail .com>
2020-05-03 00:28:52 +0600

    True vs False

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//Falsy:
+//false
+// 0
+//NaN
+//""
+//null
+//undefined
+
+//Truly:
+//" ", "0",[ ]
+//true
+
+const age = 4;
+if (age>0) {
+    console.log("Condition is true")
+    
+}
+else{
+    console.log("COndition is False")
+}
