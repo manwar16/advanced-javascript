0a66433b7a51e73b81a0ade7609c81b47cef129e
manwar16 <mdmanwarcse16@gmail .com>
2020-05-03 02:44:20 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,27 @@
+//System1
+// let pakhi;
+// console.log(pakhi);
+
+//System2
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return ;
+// }
+// const result = add(12,83);
+// console.log(result);
+
+//System3
+// function add(num1, num2) {
+//     console.log(num1,num2);
+    
+// }
+// const result = add(13);
+// console.log(result);
+
+//System4
+
+const student = {name:'Mono', phone:2441139}
+console.log(student.address);
+
+const fun = undefined;
+console.log(fun);
