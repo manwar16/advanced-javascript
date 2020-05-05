cc2ccbaeb07d22787cd4b6b86024534642680bbc
manwar16 <mdmanwarcse16@gmail .com>
2020-05-05 11:11:00 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,14 @@
+let bonus = 20;
+function sum(first, second) {
+    let result = first + second + bonus;
+    //return result;
+    if (result > 9) {
+        let mood = "happy";
+        mood ='Fishy';
+        console.log(mood);
+    }
+}
+
+// console.log(result);
+const output = sum(3, 7);
+console.log(output);
