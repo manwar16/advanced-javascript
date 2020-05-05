347e2b65c96524bf300cb3a42ed826a7c24dc65e
manwar16 <mdmanwarcse16@gmail .com>
2020-05-05 22:40:03 +0600

    Array Slice,splice,join

---
+++ array-split.js
@@ -0,0 +1,17 @@
+//slice
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+// console.log(part);
+// console.log(nums);
+
+//splice
+const numbers = [11, 22, 33, 44, 55, 66, 77, 88];
+const removed = numbers.splice(2, 5);
+console.log(removed);
+console.log(numbers);
+
+//join
+const joinNums = [11, 22, 33, 44, 55, 66, 77, 88];
+const  together = joinNums.join("");
+console.log(together);
+
