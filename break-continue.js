34481fd303cd27a6958faa93d3ba4b1ebe73e45c
manwar16 <mdmanwarcse16@gmail .com>
2020-05-05 23:19:38 +0600

    break, continue

--- break-continue.js
+++ break-continue.js
@@ -1,5 +1,19 @@
+//Break
 const nums = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let i = 0; i < nums.length; i++) {
     const element = nums[i];
+    if(element > 5){
+        break;
+    }
+    console.log(element);
+}
+
+//Continue
+const joinNums = [11, 22, -33, 44, -55, 66, -77, 88];
+for (let i = 0; i < joinNums.length; i++) {
+    const element = joinNums[i];
+    if(element < 0){
+        continue;
+    }
     console.log(element);
 }
